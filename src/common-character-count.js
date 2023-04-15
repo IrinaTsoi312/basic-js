0d40d1a582f17929dcebf0d16a40dac330cea283
Irina Tsoi <irinatsoi312@gmail.com>
2023-04-16 02:17:46 +0600

    feat: add solution to common-character-count task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  const str1 = s1.split("");
+  const str2 = s2.split("");
+  for(let i=0; i<str1.length; i++) {
+    for(let k=0; k<str2.length; k++) {
+      if(str1[i] === str2[k]) {
+        str2.splice(k, 1);
+        count++;
+        break;
+      }
+      else { continue; }
+    }
+  }
+  return count;
 }
 
 module.exports = {
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = [];
+  // const optionsList = Object.keys(options);
+  // console.log(optionList);
+  let n1, substr, s = "+", additionSeparator = "|", n2;
+  function repeatTimes(str, n1) {
+    n1 = options.repeatTimes;
+    for(let i=0; i<n1; i++) {
+      result.push(str);
+    }
+    if(optionsList.includes("additionRepeatTimes")) {
+      function additionRepeatTimes(substr, additionSeparator, n2) {
+        const arr = [];
+        for(let i=0; i<n2; i++) {
+          arr.push(substr);
+        }
+        for(let k=0; k<result.length; k++) {
+          if(optionsList.includes("additionSeparator")) {
+            result[k] = result[k] += arr.join(`${additionSeparator}`);            
+          }
+          else {
+            for(let k=0; k<result.length; k++) {
+              result[k] = result[k] += arr.join("");
+            }
+          }
+        }
+        
+      }
+    }
+  };
+  function seperator(arr, s) {
+    return arr.join(`${s}`);
+  };
+  
+  return result;
 }
-
+console.log(repeater("STRING", { repeatTimes: 3, separator: '**', 
+addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' }));
 module.exports = {
   repeater
 };
